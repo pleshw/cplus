04c4aafbe349854e4c517bcaf680779733958806
pleshw <pleshw@outlook.com>
2018-09-06 12:56:15 -0300

    Developing sort algorithms to EDB grade.

---
+++ include/sort.hpp
@@ -0,0 +1,17 @@
+#ifndef SORTING_EDB
+#define SORTING_EDB
+
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+void selection_sort(int * begin, int * end);
+void insertion_sort(int * begin, int * end);
+void bubble_sort(int * begin, int * end);
+void merge_sort(int * begin, int * end);
+void quick_sort(int * begin, int * end);
+
+
+
+
+#endif
---
+++ src/sort.cpp
@@ -0,0 +1,14 @@
+#include "../include/sort.h"
+
+
+void selection_sort(int * begin, int * end){
+	
+}
+
+void insertion_sort(int * begin, int * end){}
+
+void bubble_sort(int * begin, int * end){}
+
+void merge_sort(int * begin, int * end){}
+
+void quick_sort(int * begin, int * end){}
